2335a61cc7762a1c761adf89626b8dbdef148bb2
sahil-123455 <sg2145984@gmail.com>
2025-09-27 20:21:38 +0530

    Updated tagline and site text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,8 @@ const Footer = () => {
                 <div className="md:max-w-96">
                     <img alt="" class="h-11" src={assets.logo} />
                     <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       Book your favourite shows in seconds.
+QuickShow lets you discover movies, book tickets, and enjoy seamless entertainment—anytime, anywhere.
                     </p>
                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto " />
